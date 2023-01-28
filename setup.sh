6eba9e422e27a7e14f2b3a3d6827092cf3dd1714
Alex Romine <Alex.Romine@Alex.Romine>
2023-01-27 22:21:02 -0700

    updates

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew install zsh
+
+CURRENT_DIR="$( dirname -- "${BASH_SOURCE[0]}" )"
+
+ln -sf ${CURRENT_DIR}/zshrc ~/.zshrc
+ln -sf ${CURRENT_DIR}/vimrc ~/.vimrc
+ln -sf ${CURRENT_DIR}/tmux.conf ~/.tmux.conf
+ln -sf ${CURRENT_DIR}/vimrc ~/.vimrc
+ln -sf ${CURRENT_DIR}/gitconfig ~/.gitconfig

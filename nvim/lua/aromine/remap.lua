3edc17d713563977ae4e02f81753f466206dce78
Alex Romine <alex.romine@uplight.com>
2023-07-25 17:12:43 -0600

    fix .

--- nvim/lua/aromine/remap.lua
+++ nvim/lua/aromine/remap.lua
@@ -33,7 +33,7 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Fix . 
-vim.keymap.set("n", ".", [[.]], { silent = true })
+vim.keymap.set("n", ".", [[1.]], { silent = true })
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")

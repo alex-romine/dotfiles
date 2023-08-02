33679e3b3254ce2cdfd6cd5a45206887f81364d9
Alex Romine <alex.romine@uplight.com>
2023-08-01 18:55:47 -0600

    fzf for telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -17,9 +17,21 @@ telescope.setup {
             hidden = true,
         },
         live_grep = {
-            additional_args = function(opts)
+            additional_args = function()
                 return {"--hidden"}
             end
         },
+    },
+    extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
     }
+  }
 }
+
+require('telescope').load_extension('fzf')
+
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -185,6 +185,11 @@ _G.packer_plugins = {
     path = "/Users/Alex.Romine/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["telescope-fzf-native.nvim"] = {
+    loaded = true,
+    path = "/Users/Alex.Romine/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
+    url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim"
+  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/Users/Alex.Romine/.local/share/nvim/site/pack/packer/start/telescope.nvim",

f7f838c95880b51b5fa1cc184345c53db246fe3f
Alex Romine <alex.romine@uplight.com>
2023-09-08 12:05:13 -0600

    fix treesitter

--- nvim/lua/aromine/packer.lua
+++ nvim/lua/aromine/packer.lua
@@ -27,7 +27,6 @@ return require('packer').startup(function(use)
     })
 
     use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use ('nvim-treesitter/playground')
     use ('mbbill/undotree')
 
     -- Show folders like IDE
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -175,11 +175,6 @@ _G.packer_plugins = {
     path = "/Users/Alex.Romine/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
-  playground = {
-    loaded = true,
-    path = "/Users/Alex.Romine/.local/share/nvim/site/pack/packer/start/playground",
-    url = "https://github.com/nvim-treesitter/playground"
-  },
   ["plenary.nvim"] = {
     loaded = true,
     path = "/Users/Alex.Romine/.local/share/nvim/site/pack/packer/start/plenary.nvim",
